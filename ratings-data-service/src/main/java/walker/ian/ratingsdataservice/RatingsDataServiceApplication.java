d3ecf4ff503c38dff20d9df084968031386ed9a3
walkergb <ianhwalker@me.com>
2019-12-04 14:56:39 -0500

    Initial commit of service skeletons

---
+++ ratings-data-service/src/main/java/walker/ian/ratingsdataservice/RatingsDataServiceApplication.java
@@ -0,0 +1,13 @@
+package walker.ian.ratingsdataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingsDataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatingsDataServiceApplication.class, args);
+	}
+
+}
---
+++ ratings-data-service/src/test/java/walker/ian/ratingsdataservice/RatingsDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package walker.ian.ratingsdataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingsDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
